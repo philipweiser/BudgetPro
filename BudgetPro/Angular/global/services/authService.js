28a366c1a986d4108aa957d2f6464cf7687837d2
philipweiser <philipkrw@gmail.com>
2015-03-26 16:29:11 -0400

    mo

--- BudgetPro/Angular/global/services/authService.js
+++ BudgetPro/Angular/global/services/authService.js
@@ -1,82 +1,89 @@
-﻿(function () {
-    'use strict';
-    angular.module('app').factory('authService', ['$http', '$q', 'localStorageService', function ($http, $q, localStorageService) {
+﻿'use strict';
+angular.module('app').factory('authService', ['$http', '$q', 'localStorageService', function ($http, $q, localStorageService) {
 
-        var serviceBase = '/';
-        var authServiceFactory = {};
+    var serviceBase = '/';
+    var authServiceFactory = {};
 
-        var _authentication = {
-            isAuth: false,
-            username: "",
-            token: "",
-        };
+    var _authentication = {
+        isAuth: false,
+        name: "",
+        username: "",
+        token: "",
+        roles: []
+    };
 
-        var _register = function (registration) {
+    var _register = function (registration) {
 
+        _logOut();
+
+        return $http.post('/api/Account/register', registration).then(function (response) {
+            return response;
+        });
+
+    };
+
+    var _login = function (loginData) {
+
+        var data = "grant_type=password&username=" + loginData.Email + "&password=" + loginData.Password;
+
+        var deferred = $q.defer();
+
+        $http.post('/token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function (response) {
+
+            _authentication.isAuth = true;
+            _authentication.username = response.Username;
+            _authentication.token = response.access_token;
+            _authentication.name = response.Name;
+            _authentication.roles = response.Roles;
+
+            localStorageService.set('authorizationData', _authentication);
+
+            deferred.resolve(response);
+
+        }).error(function (err, status) {
             _logOut();
+            deferred.reject(err);
+        });
 
-            return $http.post('/api/Account/register', registration).then(function (response) {
-                return response;
-            });
+        return deferred.promise;
 
-        };
+    };
 
-        var _login = function (loginData) {
+    authServiceFactory.isAuth = function () {
+        console.log(authServiceFactory.authentication);
+        return authServiceFactory.authentication.isAuth;
+    }
 
-            var data = "grant_type=password&username=" + loginData.Email + "&password=" + loginData.Password;
+    var _logOut = function () {
 
-            var deferred = $q.defer();
+        localStorageService.remove('authorizationData');
 
-            $http.post('/token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function (response) {
+        _authentication.isAuth = false;
+        _authentication.username = "";
+        _authentication.name = "";
+        _authentication.token = "";
+        _authentication.roles = [];
 
-                _authentication.isAuth = true;
-                _authentication.username = response.userName;
-                _authentication.token = response.access_token;
+    };
 
-                localStorageService.set('authorizationData', _authentication);
+    var _fillAuthData = function () {
 
-                deferred.resolve(response);
-
-            }).error(function (err, status) {
-                _logOut();
-                deferred.reject(err);
-            });
-
-            return deferred.promise;
-
-        };
-
-        var _logOut = function () {
-
-            localStorageService.remove('authorizationData');
-
-            _authentication.isAuth = false;
-            _authentication.userName = "";
-            _authentication.name = "";
-            _authentication.claims = null;
-            _authentication.token = "";
-
-        };
-
-        var _fillAuthData = function () {
-
-            var authData = localStorageService.get('authorizationData');
-            if (authData) {
-                _authentication.isAuth = true;
-                _authentication.username = authData.userName;
-                _authentication.token = authData.token;
-            }
-
+        var authData = localStorageService.get('authorizationData');
+        if (authData) {
+            _authentication.isAuth = true;
+            _authentication.username = authData.userName;
+            _authentication.name = authData.name;
+            _authentication.token = authData.token;
+            _authentication.roles = authData.roles;
         }
 
-        authServiceFactory.register = _register;
-        authServiceFactory.login = _login;
-        authServiceFactory.logOut = _logOut;
-        authServiceFactory.fillAuthData = _fillAuthData;
-        authServiceFactory.authentication = _authentication;
+    }
 
-        return authServiceFactory;
-    }]);
+    authServiceFactory.register = _register;
+    authServiceFactory.login = _login;
+    authServiceFactory.logout = _logOut;
+    authServiceFactory.fillAuthData = _fillAuthData;
+    authServiceFactory.authentication = _authentication;
 
-
-})();
+    return authServiceFactory;
+}]);
