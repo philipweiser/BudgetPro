3e68d610b93de03067d388ee2237bf87f6a123c8
philipweiser <philipkrw@gmail.com>
2015-04-06 17:43:11 -0400

    Budgeting page modals working

--- BudgetPro/Angular/Areas/budget/budgetController.js
+++ BudgetPro/Angular/Areas/budget/budgetController.js
@@ -1,19 +1,17 @@
 ﻿angular.module('app')
     // Path: /
-    .controller('budgetController', ['$scope', '$state', '$stateParams', 'budgetItemSvcs', 'categorySvcs', '$location', function ($scope, $state, $stateParams, budgetItemSvcs, categorySvcs, $location) {
+    .controller('budgetController', ['$scope', '$state', '$stateParams', 'budgetItemSvcs', 'categorySvcs', '$location', '$modal', function ($scope, $state, $stateParams, budgetItemSvcs, categorySvcs, $location, $modal) {
         $scope.budgetItems = [];
         $scope.getBudget = function () {
             budgetItemSvcs.getBudget()
                 .then(function (response) {
-                    console.log(response);
-                    if (response.data != undefined) {
+                    if (response != undefined) {
                         $scope.budgetItems = response.data;
                     } else {
                         $location.path("/Household");
                     }
                 });
         };
-        $scope.isDebit = true;
         $scope.getBudget();
         $scope.budgetItem = {
             Id: '',
@@ -29,7 +27,7 @@
             name: '',
             width: '80', field: 'Id',
             cellTemplate: '<div class="ui-grid-cell-contents" title="Edit Budget Item">' +
-                '<input type="button"  ng-click="grid.appScope.whichEdit(row.entity)" class="btn btn-primary" value="Edit" /></div>',
+                '<input type="button"  ng-click="grid.appScope.editModal(row.entity)" class="btn btn-primary" value="Edit" /></div>',
             enableSorting: false,
             enableFiltering: false,
             enableColumnMenu: false,
@@ -46,11 +44,8 @@
             { name: 'Description' },
             { name: 'Category', field: 'CategoryName' },
             { name: 'Amount', cellFilter:'currency' },
-            { name: 'Frequency' }
+            { name: 'Frequency/Year', field: 'Frequency' }
         ];
-        $scope.whichEdit = function (entity) {
-            $scope.budgetItem = entity;
-        }
         $scope.gridOptions = {
             data: 'budgetItems',
             columnDefs: $scope.columns,
@@ -65,9 +60,6 @@
             });
         }
         $scope.createBudget = function () {
-            if ($scope.isDebit) {
-                $scope.budgetItem.Amount = -$scope.budgetItem.Amount;
-            }
             budgetItemSvcs.createBudgetItem($scope.budgetItem)
                 .then(function (response) {
                     $scope.getBudget();
@@ -95,4 +87,98 @@
             $scope.budgetItem.CategoryName = '';
             $scope.budgetItem.Frequency = '';
         }
+
+        $scope.newModal = function () {
+            $scope.modalInstance = $modal.open({
+                templateUrl: '/Angular/Areas/budget/createModal.html',
+                size: 'sm',
+                controller: 'newBIModalController',
+                resolve: {
+                    categories: function () {
+                        return $scope.categories;
+                    }
+                }
+            });
+            $scope.modalInstance.result.then(function (response) {
+                if (response != undefined) {
+                    $scope.budgetItem = response;
+                    $scope.createBudget();
+                }
+            }
+        )
+        };
+        $scope.editModal = function (entity) {
+            $scope.modalInstance = $modal.open({
+                templateUrl: '/Angular/Areas/budget/editModal.html',
+                size: 'sm',
+                controller: 'editBIModalController',
+                resolve: {
+                    entity: function () {
+                        return entity;
+                    },
+                    categories: function(){
+                        return $scope.categories;
+                    }
+                }
+            });
+            $scope.modalInstance.result.then(function (response) {
+                if (response != undefined) {
+                    $scope.budgetItem = response;
+                    $scope.updateBudgetItem();
+                }
+            }
+        )
+        };
     }])
+
+angular.module('app').controller('newBIModalController', function ($scope, $modalInstance, categories) {
+    $scope.categories = categories;
+    $scope.isComplete = false;
+    $scope.budgetItem = {
+        Description: '',
+        Amount: '',
+        Frequency: '',
+        CategoryName: '',
+        isDebit: true,
+    };
+    $scope.isDebit = true;
+    $scope.ok = function () {
+        if ($scope.budgetItem.Description != '' &&
+            $scope.budgetItem.CategoryName != '' &&
+            $scope.budgetItem.Frequency != '' &&
+            $scope.budgetItem.CategoryName != '')
+            $scope.isComplete = true;
+        if ($scope.isComplete) {
+            if ($scope.budgetItem.isDebit) {
+                $scope.budgetItem.Amount = -$scope.budgetItem.Amount;
+            }
+            $scope.budgetItem.CategoryId = $scope.budgetItem.CategoryName.Name.Id;
+            $scope.budgetItem.CategoryName = $scope.budgetItem.CategoryName.Name.Name;
+            $modalInstance.close($scope.budgetItem);
+        }
+    };
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});
+angular.module('app').controller('editBIModalController', function ($scope, $modalInstance, entity, categories) {
+    $scope.budgetItem = entity;
+    $scope.categories = categories;
+    $scope.isDebit = false;
+    if ($scope.budgetItem.Amount < 0) {
+        $scope.isDebit = true;
+    }
+    $scope.ok = function () {
+        if ($scope.budgetItem != undefined) {
+            if ($scope.isDebit == true) {
+                $scope.budgetItem.Amount = -Math.abs($scope.budgetItem.Amount);
+            } else {
+                $scope.budgetItem.Amount = Math.abs($scope.budgetItem.Amount);
+            }
+            $modalInstance.close($scope.budgetItem);
+        }
+    };
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});
